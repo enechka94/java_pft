132e429788bded23fee955ab175cb384b44a5132
enechka94 <enechka94@gmail.com>
2019-06-26 20:27:50 +0300

    first tests created

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class PointTests {
+    @Test
+
+    public void testDistance() {
+        Point p1 = new Point(1, 3);
+        Point p2 = new Point(1,7);
+        Assert.assertEquals(p2.distance(p1),9);
+        assert p1.distance(p2) == 0;
+    }
+}
