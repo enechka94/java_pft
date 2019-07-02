f7b06c8ce34a752251a524b879ce8531e5f15557
enechka94 <enechka94@gmail.com>
2019-07-02 19:40:17 +0300

    methods created

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
@@ -16,11 +16,141 @@ public class ContactCreationTest {
   public void setUp() throws Exception {
     wb = new ChromeDriver();
     wb.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wb.get("http://localhost/addressbook/index.php");
+    login();
   }
 
   @Test
   public void testContactCreation() throws Exception {
-    wb.get("http://localhost/addressbook/index.php");
+
+    addNewContact();
+    fillFirstName();
+    fillMiddleName();
+    fillLastName();
+    fillNickname();
+    fillCompanyName();
+    fillAddress();
+    fillHomePhoneNumber();
+    fillMobileNumber();
+    fillWorkPhoneNumber();
+    fillEmail1();
+    fillEmail2();
+    fillEmail3();
+    chooseBday();
+    chooseBmonth();
+    chooseByear();
+    clickEnter();
+    goToHomePage();
+
+  }
+
+  private void logout() {
+    wb.findElement(By.linkText("Logout")).click();
+  }
+
+  private void goToHomePage() {
+    wb.findElement(By.linkText("home page")).click();
+  }
+
+  private void clickEnter() {
+    wb.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]")).click();
+  }
+
+  private void chooseByear() {
+    wb.findElement(By.name("byear")).click();
+    wb.findElement(By.name("byear")).clear();
+    wb.findElement(By.name("byear")).sendKeys("1995");
+  }
+
+  private void chooseBmonth() {
+    wb.findElement(By.name("bmonth")).click();
+    new Select(wb.findElement(By.name("bmonth"))).selectByVisibleText("October");
+    wb.findElement(By.name("bmonth")).click();
+  }
+
+  private void chooseBday() {
+    wb.findElement(By.name("bday")).click();
+    new Select(wb.findElement(By.name("bday"))).selectByVisibleText("19");
+    wb.findElement(By.name("bday")).click();
+  }
+
+  private void fillEmail3() {
+    wb.findElement(By.name("email3")).click();
+    wb.findElement(By.name("email3")).clear();
+    wb.findElement(By.name("email3")).sendKeys("anna2@yandex.ru");
+  }
+
+  private void fillEmail2() {
+    wb.findElement(By.name("email2")).click();
+    wb.findElement(By.name("email2")).clear();
+    wb.findElement(By.name("email2")).sendKeys("anna1@gmail.com");
+  }
+
+  private void fillEmail1() {
+    wb.findElement(By.name("email")).click();
+    wb.findElement(By.name("email")).clear();
+    wb.findElement(By.name("email")).sendKeys("anna@mail.ru");
+  }
+
+  private void fillWorkPhoneNumber() {
+    wb.findElement(By.name("work")).click();
+    wb.findElement(By.name("work")).clear();
+    wb.findElement(By.name("work")).sendKeys("+3457585");
+  }
+
+  private void fillMobileNumber() {
+    wb.findElement(By.name("mobile")).click();
+    wb.findElement(By.name("mobile")).clear();
+    wb.findElement(By.name("mobile")).sendKeys("+2564572");
+  }
+
+  private void fillHomePhoneNumber() {
+    wb.findElement(By.name("home")).click();
+    wb.findElement(By.name("home")).clear();
+    wb.findElement(By.name("home")).sendKeys("+2562762");
+  }
+
+  private void fillAddress() {
+    wb.findElement(By.name("address")).click();
+    wb.findElement(By.name("address")).clear();
+    wb.findElement(By.name("address")).sendKeys("12 gfgb 45");
+  }
+
+  private void fillCompanyName() {
+    wb.findElement(By.name("company")).click();
+    wb.findElement(By.name("company")).clear();
+    wb.findElement(By.name("company")).sendKeys("lala");
+  }
+
+  private void fillNickname() {
+    wb.findElement(By.name("nickname")).click();
+    wb.findElement(By.name("nickname")).clear();
+    wb.findElement(By.name("nickname")).sendKeys("anna1");
+  }
+
+  private void fillLastName() {
+    wb.findElement(By.name("lastname")).click();
+    wb.findElement(By.name("lastname")).clear();
+    wb.findElement(By.name("lastname")).sendKeys("Skvortsova");
+  }
+
+  private void fillMiddleName() {
+    wb.findElement(By.name("middlename")).click();
+    wb.findElement(By.name("middlename")).clear();
+    wb.findElement(By.name("middlename")).sendKeys("John");
+  }
+
+  private void fillFirstName() {
+    wb.findElement(By.name("firstname")).click();
+    wb.findElement(By.name("firstname")).clear();
+    wb.findElement(By.name("firstname")).sendKeys("Anna");
+  }
+
+  private void addNewContact() {
+    wb.findElement(By.linkText("add new")).click();
+  }
+
+  private void login() {
     wb.findElement(By.name("user")).click();
     wb.findElement(By.name("user")).clear();
     wb.findElement(By.name("user")).sendKeys("admin");
@@ -28,61 +158,11 @@ public class ContactCreationTest {
     wb.findElement(By.name("pass")).clear();
     wb.findElement(By.name("pass")).sendKeys("secret");
     wb.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
-    wb.findElement(By.linkText("add new")).click();
-    wb.findElement(By.name("firstname")).click();
-    wb.findElement(By.name("firstname")).clear();
-    wb.findElement(By.name("firstname")).sendKeys("Anna");
-    wb.findElement(By.name("middlename")).click();
-    wb.findElement(By.name("middlename")).clear();
-    wb.findElement(By.name("middlename")).sendKeys("John");
-    wb.findElement(By.name("lastname")).click();
-    wb.findElement(By.name("lastname")).clear();
-    wb.findElement(By.name("lastname")).sendKeys("Skvortsova");
-    wb.findElement(By.name("nickname")).click();
-    wb.findElement(By.name("nickname")).clear();
-    wb.findElement(By.name("nickname")).sendKeys("anna1");
-    wb.findElement(By.name("title")).click();
-    wb.findElement(By.name("theform")).click();
-    wb.findElement(By.name("company")).click();
-    wb.findElement(By.name("company")).clear();
-    wb.findElement(By.name("company")).sendKeys("lala");
-    wb.findElement(By.name("address")).click();
-    wb.findElement(By.name("address")).clear();
-    wb.findElement(By.name("address")).sendKeys("12 gfgb 45");
-    wb.findElement(By.name("home")).click();
-    wb.findElement(By.name("home")).clear();
-    wb.findElement(By.name("home")).sendKeys("+2562762");
-    wb.findElement(By.name("mobile")).click();
-    wb.findElement(By.name("mobile")).clear();
-    wb.findElement(By.name("mobile")).sendKeys("+2564572");
-    wb.findElement(By.name("work")).click();
-    wb.findElement(By.name("work")).clear();
-    wb.findElement(By.name("work")).sendKeys("+3457585");
-    wb.findElement(By.name("email")).click();
-    wb.findElement(By.name("email")).clear();
-    wb.findElement(By.name("email")).sendKeys("anna@mail.ru");
-    wb.findElement(By.name("email2")).click();
-    wb.findElement(By.name("email2")).clear();
-    wb.findElement(By.name("email2")).sendKeys("anna1@gmail.com");
-    wb.findElement(By.name("email3")).click();
-    wb.findElement(By.name("email3")).clear();
-    wb.findElement(By.name("email3")).sendKeys("anna2@yandex.ru");
-    wb.findElement(By.name("bday")).click();
-    new Select(wb.findElement(By.name("bday"))).selectByVisibleText("19");
-    wb.findElement(By.name("bday")).click();
-    wb.findElement(By.name("bmonth")).click();
-    new Select(wb.findElement(By.name("bmonth"))).selectByVisibleText("October");
-    wb.findElement(By.name("bmonth")).click();
-    wb.findElement(By.name("byear")).click();
-    wb.findElement(By.name("byear")).clear();
-    wb.findElement(By.name("byear")).sendKeys("1995");
-    wb.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]")).click();
-    wb.findElement(By.linkText("home page")).click();
-    wb.findElement(By.linkText("Logout")).click();
   }
 
   @AfterClass(alwaysRun = true)
   public void tearDown() throws Exception {
+    logout();
     wb.quit();
 
   }
