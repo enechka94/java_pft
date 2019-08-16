bd73866d833306307c59381ee9d1cd454d5cf1b5
enechka94 <enechka94@gmail.com>
2019-08-16 20:25:58 +0300

    xml format for contacts done

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -1,26 +1,43 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTest extends TestBase{
+    @DataProvider
+    public Iterator<Object[]> validContactsFromXml() throws IOException {
+        String xml = "";
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+        String line = reader.readLine();
+        while (line != null) {
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xStream = new XStream();
+        xStream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>) xStream.fromXML(xml);
+        return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
 
-  @Test
-  public void testContactCreation() throws Exception {
+  @Test(dataProvider = "validContactsFromXml")
+  public void testContactCreation(ContactData contact) throws Exception {
 
    Contacts before = app.contact().all();
-      File photo = new File("src/test/resources/1556232387135-31.jpg");
-    ContactData contact = new ContactData().withFirstname("Anna").withLastname("Skvortsova").withAddress("353 fdvgdfv sfsf")
-      .withHomeNumber("+353").withMobileNumber("131 4124").withWorkNumber("3453453453(22)")
-            .withEmail1("3434@fvfdd.dv").withEmail2("434@fdv.fdv").withEmail3("4@e.2")
-            .withBday("19").withBmonth("July").withByear("1945").withGroup("test1").withPhoto(photo);
     app.contact().create(contact, true);
     app.goTo().homePage();
     Contacts after = app.contact().all();
