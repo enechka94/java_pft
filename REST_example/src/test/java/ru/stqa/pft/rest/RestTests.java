ec74f620dbc68d81b4416f418a04f1fd4c4ce7ed
enechka94 <enechka94@gmail.com>
2019-08-27 00:21:29 +0300

    task 20 completed

--- REST_example/src/test/java/ru/stqa/pft/rest/RestTests.java
+++ REST_example/src/test/java/ru/stqa/pft/rest/RestTests.java
@@ -1,13 +1,5 @@
 package ru.stqa.pft.rest;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
-import org.apache.http.client.fluent.Executor;
-import org.apache.http.client.fluent.Request;
-import org.apache.http.message.BasicNameValuePair;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -16,12 +8,16 @@ import java.util.Set;
 
 import static org.testng.Assert.*;
 
-public class RestTests {
+public class RestTests extends TestBase{
 
     @Test
 
     public void testCreateIssue() throws IOException {
         Set<Issue> oldIssues = getIssues();
+        int checkedIssueId = 1700; //1710 - ignored
+        if(isIssueOpen(checkedIssueId)) {
+            System.out.println("Ignored because of issue " + checkedIssueId);}
+        skipIfNotFixed(checkedIssueId);
         Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");
         int issueId = create(newIssue);
         Set<Issue> newIssues = getIssues();
@@ -29,24 +25,4 @@ public class RestTests {
         assertEquals(oldIssues, newIssues);
     }
 
-    private Set<Issue> getIssues() throws IOException {
-        String json = getExecutor().execute(Request.Get("http://bugify.stqa.ru/api/issues.json?limit=2000"))
-                .returnContent().asString();
-        JsonElement parsed = new JsonParser().parse(json);
-        JsonElement issues = parsed.getAsJsonObject().get("issues");
-        return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
-    }
-
-    private Executor getExecutor() {
-        return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
-    }
-
-    private int create(Issue newIssue) throws IOException {
-        String json = getExecutor().execute(Request.Post("https://bugify.stqa.ru/api/issues.json?limit=2000")
-                .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
-                        new BasicNameValuePair("description", newIssue.getDescription())))
-                .returnContent().asString();
-        JsonElement parsed = new JsonParser().parse(json);
-        return parsed.getAsJsonObject().get("issue_id").getAsInt();
-    }
 }
